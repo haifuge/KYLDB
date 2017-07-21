50cf9e48ffa5a42b408ea4efb82f31b6043ec64d
haifuge <ge.haifu@gmail.com>
2017-07-21 09:13:30 -0400

    1

--- KYLDB/Reports/StatisticReport/StatisticReport.cs
+++ KYLDB/Reports/StatisticReport/StatisticReport.cs
@@ -34,83 +34,135 @@ namespace KYLDB.Reports.StatisticReport
 
         private void StatisticReport_Load(object sender, EventArgs e)
         {
-            string sql = @"select *, I+P+C+CS as 'Total' from (
-                            select r.Rep, count(cdi.Rep) as 'I', count(cdp.Rep) as 'P', count(cdc.Rep) as 'C', COUNT(cdcs.Rep) as 'CS'
-                            from Representative r 
-                            left join ClientDetail cdi on r.Rep=cdi.Rep and cdi.CustomerType in ('Individual', 'SINGLE MEMBER LLC', 'SOLE PROPRIETORSHIP')
-                            left join ClientDetail cdp on r.rep=cdp.Rep and cdp.CustomerType in ('GENERAL PARTNER LLC','PARTNERSHIP','Limited Partnership')
-                            left join ClientDetail cdc on r.Rep=cdc.Rep and cdc.CustomerType in ('CORP.','CORP.LLC','Corporate')
-                            left join ClientDetail cdcs on r.Rep=cdcs.Rep and cdcs.CustomerType in ('S Corp.')
-                            group by r.Rep) a";
+            string sql = @"select i.Rep, i.I, p.P, c.C, cs.CS, i.I+ p.P+c.C+cs.CS as 'Total' from
+                            (select r.Rep, count(cdi.Rep) as 'I'
+                            from Representative r left join ClientDetail cdi on r.Rep=cdi.Rep and cdi.CustomerType in ('Individual', 'SINGLE MEMBER LLC', 'SOLE PROPRIETORSHIP')
+                            group by r.Rep) i,
+                            (select r.Rep,count(cdp.Rep) as 'P'
+                            from Representative r left join ClientDetail cdp on r.rep=cdp.Rep and cdp.CustomerType in ('GERNERAL PARTNER LLC','PARTNERSHIP','Limited Partnership')
+                            group by r.Rep) p,
+                            (select r.Rep,count(cdc.Rep) as 'C'
+                            from Representative r left join ClientDetail cdc on r.Rep=cdc.Rep and cdc.CustomerType in ('CORP.','CORP.LLC','Corporate')
+                            group by r.Rep) c,
+                            (select r.Rep, COUNT(cdcs.Rep) as 'CS'
+                            from Representative r left join ClientDetail cdcs on r.Rep=cdcs.Rep and cdcs.CustomerType in ('S Corp.')
+                            group by r.Rep) cs
+                            where i.Rep=p.Rep and p.Rep=c.Rep and c.Rep=cs.Rep";
             DataTable dt = DBOperator.QuerySql(sql);
             TOTALBindingSource.DataSource = dt;
 
-            sql = @"select r.Rep, count(st.Rep) as 'ST', count(pr.Rep) as 'PR', count(liq.Rep) as 'Liquor', COUNT(uo.Rep) as 'UO'
-                    from Representative r 
-                    left join clientDetail st on r.Rep=st.Rep and st.SalesTax='Yes'
-                    left join ClientDetail pr on r.rep=pr.Rep and pr.Payroll like 'Yes%'
-                    left join clientDetail liq on r.Rep=liq.Rep and liq.LiquorTax_Phila like 'Yes%'
-                    left join ClientDetail uo on r.Rep=uo.Rep and uo.U_OTax like 'Yes%'
-                    group by r.Rep";
+            sql = @"select i.Rep, i.ST, p.PR, c.Liquor, cs.UO, i.ST+p.PR+c.Liquor+cs.UO as 'Total' from
+                    (select r.Rep, count(st.Rep) as 'ST'
+                    from Representative r left join ClientDetail st on r.Rep=st.Rep and st.SalesTax='Yes'
+                    group by r.Rep) i,
+                    (select r.Rep,count(pr.Rep) as 'PR'
+                    from Representative r left join ClientDetail pr on r.rep=pr.Rep and pr.Payroll like 'Yes%'
+                    group by r.Rep) p,
+                    (select r.Rep,count(liq.Rep) as 'Liquor'
+                    from Representative r left join ClientDetail liq on r.Rep=liq.Rep and liq.LiquorTax_Phila like 'Yes%'
+                    group by r.Rep) c,
+                    (select r.Rep, COUNT(uo.Rep) as 'UO'
+                    from Representative r left join ClientDetail uo on r.Rep=uo.Rep and uo.U_OTax like 'Yes%'
+                    group by r.Rep) cs
+                    where i.Rep=p.Rep and p.Rep=c.Rep and c.Rep=cs.Rep";
             dt = DBOperator.QuerySql(sql);
             STPRBindingSource.DataSource = dt;
 
-            sql = @"select *, I+P+C+CS as 'Total' from (
-                        select r.Rep, count(cdi.Rep) as 'I', count(cdp.Rep) as 'P', count(cdc.Rep) as 'C', COUNT(cdcs.Rep) as 'CS'
-                        from Representative r 
-                        left join ClientDetail cdi on r.Rep=cdi.Rep and cdi.JobStatus = 'Pending' and cdi.CustomerType in ('Individual', 'SINGLE MEMBER LLC', 'SOLE PROPRIETORSHIP')
-                        left join ClientDetail cdp on r.rep=cdp.Rep and cdp.JobStatus = 'Pending' and cdp.CustomerType in ('GENERAL PARTNER LLC','PARTNERSHIP','Limited Partnership')
-                        left join ClientDetail cdc on r.Rep=cdc.Rep and cdc.JobStatus = 'Pending' and cdc.CustomerType in ('CORP.','CORP.LLC','Corporate')
-                        left join ClientDetail cdcs on r.Rep=cdcs.Rep and cdcs.JobStatus = 'Pending' and cdcs.CustomerType in ('S Corp.')
-                        group by r.Rep) a";
+            sql = @"select i.Rep, i.I, p.P, c.C, cs.CS, i.I+ p.P+c.C+cs.CS as 'Total' from
+                    (select r.Rep, count(cdi.Rep) as 'I'
+                    from Representative r left join ClientDetail cdi on r.Rep=cdi.Rep and cdi.JobStatus = 'Pending' and cdi.CustomerType in ('Individual', 'SINGLE MEMBER LLC', 'SOLE PROPRIETORSHIP')
+                    group by r.Rep) i,
+                    (select r.Rep,count(cdp.Rep) as 'P'
+                    from Representative r left join ClientDetail cdp on r.rep=cdp.Rep and cdp.JobStatus = 'Pending' and cdp.CustomerType in ('GERNERAL PARTNER LLC','PARTNERSHIP','Limited Partnership')
+                    group by r.Rep) p,
+                    (select r.Rep,count(cdc.Rep) as 'C'
+                    from Representative r left join ClientDetail cdc on r.Rep=cdc.Rep and cdc.JobStatus = 'Pending' and cdc.CustomerType in ('CORP.','CORP.LLC','Corporate')
+                    group by r.Rep) c,
+                    (select r.Rep, COUNT(cdcs.Rep) as 'CS'
+                    from Representative r left join ClientDetail cdcs on r.Rep=cdcs.Rep and cdcs.JobStatus = 'Pending' and cdcs.CustomerType in ('S Corp.')
+                    group by r.Rep) cs
+                    where i.Rep=p.Rep and p.Rep=c.Rep and c.Rep=cs.Rep";
+            dt = DBOperator.QuerySql(sql);
             List<TOTAL> items = DBOperator.getListFromTable<TOTAL>(dt);
             ReportDataSource peding = new ReportDataSource("dsPending", items);
             reportViewer1.LocalReport.DataSources.Add(peding);
 
-            sql = @"select *, I+P+C+CS as 'Total' from (
-                        select r.Rep, count(cdi.Rep) as 'I', count(cdp.Rep) as 'P', count(cdc.Rep) as 'C', COUNT(cdcs.Rep) as 'CS'
-                        from Representative r 
-                        left join ClientDetail cdi on r.Rep=cdi.Rep and cdi.JobStatus = 'Yearly' and cdi.CustomerType in ('Individual', 'SINGLE MEMBER LLC', 'SOLE PROPRIETORSHIP')
-                        left join ClientDetail cdp on r.rep=cdp.Rep and cdp.JobStatus = 'Yearly' and cdp.CustomerType in ('GENERAL PARTNER LLC','PARTNERSHIP','Limited Partnership')
-                        left join ClientDetail cdc on r.Rep=cdc.Rep and cdc.JobStatus = 'Yearly' and cdc.CustomerType in ('CORP.','CORP.LLC','Corporate')
-                        left join ClientDetail cdcs on r.Rep=cdcs.Rep and cdcs.JobStatus = 'Yearly' and cdcs.CustomerType in ('S Corp.')
-                        group by r.Rep) a";
+            sql = @"select i.Rep, i.I, p.P, c.C, cs.CS, i.I+ p.P+c.C+cs.CS as 'Total' from
+                    (select r.Rep, count(cdi.Rep) as 'I'
+                    from Representative r left join ClientDetail cdi on r.Rep=cdi.Rep and cdi.JobStatus = 'Yearly' and cdi.CustomerType in ('Individual', 'SINGLE MEMBER LLC', 'SOLE PROPRIETORSHIP')
+                    group by r.Rep) i,
+                    (select r.Rep,count(cdp.Rep) as 'P'
+                    from Representative r left join ClientDetail cdp on r.rep=cdp.Rep and cdp.JobStatus = 'Yearly' and cdp.CustomerType in ('GERNERAL PARTNER LLC','PARTNERSHIP','Limited Partnership')
+                    group by r.Rep) p,
+                    (select r.Rep,count(cdc.Rep) as 'C'
+                    from Representative r left join ClientDetail cdc on r.Rep=cdc.Rep and cdc.JobStatus = 'Yearly' and cdc.CustomerType in ('CORP.','CORP.LLC','Corporate')
+                    group by r.Rep) c,
+                    (select r.Rep, COUNT(cdcs.Rep) as 'CS'
+                    from Representative r left join ClientDetail cdcs on r.Rep=cdcs.Rep and cdcs.JobStatus = 'Yearly' and cdcs.CustomerType in ('S Corp.')
+                    group by r.Rep) cs
+                    where i.Rep=p.Rep and p.Rep=c.Rep and c.Rep=cs.Rep";
+            dt = DBOperator.QuerySql(sql);
             items = DBOperator.getListFromTable<TOTAL>(dt);
             ReportDataSource yearly = new ReportDataSource("dsYearly", items);
             reportViewer1.LocalReport.DataSources.Add(yearly);
 
-            sql = @"select *, I+P+C+CS as 'Total' from (
-                        select r.Rep, count(cdi.Rep) as 'I', count(cdp.Rep) as 'P', count(cdc.Rep) as 'C', COUNT(cdcs.Rep) as 'CS'
-                        from Representative r 
-                        left join ClientDetail cdi on r.Rep=cdi.Rep and cdi.JobStatus = 'Current' and cdi.CustomerType in ('Individual', 'SINGLE MEMBER LLC', 'SOLE PROPRIETORSHIP')
-                        left join ClientDetail cdp on r.rep=cdp.Rep and cdp.JobStatus = 'Current' and cdp.CustomerType in ('GENERAL PARTNER LLC','PARTNERSHIP','Limited Partnership')
-                        left join ClientDetail cdc on r.Rep=cdc.Rep and cdc.JobStatus = 'Current' and cdc.CustomerType in ('CORP.','CORP.LLC','Corporate')
-                        left join ClientDetail cdcs on r.Rep=cdcs.Rep and cdcs.JobStatus = 'Current' and cdcs.CustomerType in ('S Corp.')
-                        group by r.Rep) a";
+            sql = @"select i.Rep, i.I, p.P, c.C, cs.CS, i.I+ p.P+c.C+cs.CS as 'Total' from
+                    (select r.Rep, count(cdi.Rep) as 'I'
+                    from Representative r left join ClientDetail cdi on r.Rep=cdi.Rep and cdi.JobStatus = 'Current' and cdi.CustomerType in ('Individual', 'SINGLE MEMBER LLC', 'SOLE PROPRIETORSHIP')
+                    group by r.Rep) i,
+                    (select r.Rep,count(cdp.Rep) as 'P'
+                    from Representative r left join ClientDetail cdp on r.rep=cdp.Rep and cdp.JobStatus = 'Current' and cdp.CustomerType in ('GERNERAL PARTNER LLC','PARTNERSHIP','Limited Partnership')
+                    group by r.Rep) p,
+                    (select r.Rep,count(cdc.Rep) as 'C'
+                    from Representative r left join ClientDetail cdc on r.Rep=cdc.Rep and cdc.JobStatus = 'Current' and cdc.CustomerType in ('CORP.','CORP.LLC','Corporate')
+                    group by r.Rep) c,
+                    (select r.Rep, COUNT(cdcs.Rep) as 'CS'
+                    from Representative r left join ClientDetail cdcs on r.Rep=cdcs.Rep and cdcs.JobStatus = 'Current' and cdcs.CustomerType in ('S Corp.')
+                    group by r.Rep) cs
+                    where i.Rep=p.Rep and p.Rep=c.Rep and c.Rep=cs.Rep";
+            dt = DBOperator.QuerySql(sql);
             items = DBOperator.getListFromTable<TOTAL>(dt);
             ReportDataSource current = new ReportDataSource("dsCurrent", items);
             reportViewer1.LocalReport.DataSources.Add(current);
 
-            sql = @"select *, I+P+C+CS as 'Total' from (
-                        select r.Rep, count(cdi.Rep) as 'I', count(cdp.Rep) as 'P', count(cdc.Rep) as 'C', COUNT(cdcs.Rep) as 'CS'
-                        from Representative r 
-                        left join ClientDetail cdi on r.Rep=cdi.Rep and cdi.JobStatus = 'Current' and cdi.CustomerType in ('Individual', 'SINGLE MEMBER LLC', 'SOLE PROPRIETORSHIP')
-                        left join ClientDetail cdp on r.rep=cdp.Rep and cdp.JobStatus = 'Current' and cdp.CustomerType in ('GENERAL PARTNER LLC','PARTNERSHIP','Limited Partnership')
-                        left join ClientDetail cdc on r.Rep=cdc.Rep and cdc.JobStatus = 'Current' and cdc.CustomerType in ('CORP.','CORP.LLC','Corporate')
-                        left join ClientDetail cdcs on r.Rep=cdcs.Rep and cdcs.JobStatus = 'Current' and cdcs.CustomerType in ('S Corp.')
-                        group by r.Rep) a";
+            sql = @"declare @year int
+                    set @year=Year(getdate())-1
+                    select i.Rep, i.I, p.P, c.C, cs.CS, i.I+ p.P+c.C+cs.CS as 'Total' from
+                    (select r.Rep, count(cdi.Rep) as 'I'
+                    from Representative r left join ClientDetail cdi on r.Rep=cdi.Rep and cdi.TaxPrepared = @year and cdi.CustomerType in ('Individual', 'SINGLE MEMBER LLC', 'SOLE PROPRIETORSHIP')
+                    group by r.Rep) i,
+                    (select r.Rep,count(cdp.Rep) as 'P'
+                    from Representative r left join ClientDetail cdp on r.rep=cdp.Rep and cdp.TaxPrepared = @year and cdp.CustomerType in ('GERNERAL PARTNER LLC','PARTNERSHIP','Limited Partnership')
+                    group by r.Rep) p,
+                    (select r.Rep,count(cdc.Rep) as 'C'
+                    from Representative r left join ClientDetail cdc on r.Rep=cdc.Rep and cdc.TaxPrepared = @year and cdc.CustomerType in ('CORP.','CORP.LLC','Corporate')
+                    group by r.Rep) c,
+                    (select r.Rep, COUNT(cdcs.Rep) as 'CS'
+                    from Representative r left join ClientDetail cdcs on r.Rep=cdcs.Rep and cdcs.TaxPrepared = @year and cdcs.CustomerType in ('S Corp.')
+                    group by r.Rep) cs
+                    where i.Rep=p.Rep and p.Rep=c.Rep and c.Rep=cs.Rep";
+            dt = DBOperator.QuerySql(sql);
             items = DBOperator.getListFromTable<TOTAL>(dt);
             ReportDataSource done = new ReportDataSource("dsDone", items);
             reportViewer1.LocalReport.DataSources.Add(done);
 
-            sql = @"select *, I+P+C+CS as 'Total' from (
-                        select r.Rep, count(cdi.Rep) as 'I', count(cdp.Rep) as 'P', count(cdc.Rep) as 'C', COUNT(cdcs.Rep) as 'CS'
-                        from Representative r 
-                        left join ClientDetail cdi on r.Rep=cdi.Rep and cdi.JobStatus = 'Current' and cdi.CustomerType in ('Individual', 'SINGLE MEMBER LLC', 'SOLE PROPRIETORSHIP')
-                        left join ClientDetail cdp on r.rep=cdp.Rep and cdp.JobStatus = 'Current' and cdp.CustomerType in ('GENERAL PARTNER LLC','PARTNERSHIP','Limited Partnership')
-                        left join ClientDetail cdc on r.Rep=cdc.Rep and cdc.JobStatus = 'Current' and cdc.CustomerType in ('CORP.','CORP.LLC','Corporate')
-                        left join ClientDetail cdcs on r.Rep=cdcs.Rep and cdcs.JobStatus = 'Current' and cdcs.CustomerType in ('S Corp.')
-                        group by r.Rep) a";
+            sql = @"declare @year int
+                    set @year=Year(getdate())-1
+                    select i.Rep, i.I, p.P, c.C, cs.CS, i.I+ p.P+c.C+cs.CS as 'Total' from
+                    (select r.Rep, count(cdi.Rep) as 'I'
+                    from Representative r left join ClientDetail cdi on r.Rep=cdi.Rep and (cdi.TaxPrepared <> @year or cdi.TaxPrepared='' or cdi.TaxPrepared is null) and ((cdi.JobStatus='Closed' and Year(cdi.EndDate)=@year) or (cdi.JobStatus<>'Closed' and Year(cdi.EndDate)<Year(getdate()))) and cdi.CustomerType in ('Individual', 'SINGLE MEMBER LLC', 'SOLE PROPRIETORSHIP')
+                    group by r.Rep) i,
+                    (select r.Rep,count(cdp.Rep) as 'P'
+                    from Representative r left join ClientDetail cdp on r.rep=cdp.Rep and (cdp.TaxPrepared <> @year or cdp.TaxPrepared='' or cdp.TaxPrepared is null) and ((cdp.JobStatus='Closed' and Year(cdp.EndDate)=@year) or (cdp.JobStatus<>'Closed' and Year(cdp.EndDate)<Year(getdate()))) and cdp.CustomerType in ('GERNERAL PARTNER LLC','PARTNERSHIP','Limited Partnership')
+                    group by r.Rep) p,
+                    (select r.Rep,count(cdc.Rep) as 'C'
+                    from Representative r left join ClientDetail cdc on r.Rep=cdc.Rep and (cdc.TaxPrepared <> @year or cdc.TaxPrepared='' or cdc.TaxPrepared is null) and ((cdc.JobStatus='Closed' and Year(cdc.EndDate)=@year) or (cdc.JobStatus<>'Closed' and Year(cdc.EndDate)<Year(getdate()))) and cdc.CustomerType in ('CORP.','CORP.LLC','Corporate')
+                    group by r.Rep) c,
+                    (select r.Rep, COUNT(cdcs.Rep) as 'CS'
+                    from Representative r left join ClientDetail cdcs on r.Rep=cdcs.Rep and (cdcs.TaxPrepared <> @year or cdcs.TaxPrepared='' or cdcs.TaxPrepared is null) and ((cdcs.JobStatus='Closed' and Year(cdcs.EndDate)=@year) or (cdcs.JobStatus<>'Closed' and Year(cdcs.EndDate)<Year(getdate()))) and cdcs.CustomerType in ('S Corp.')
+                    group by r.Rep) cs
+                    where i.Rep=p.Rep and p.Rep=c.Rep and c.Rep=cs.Rep";
+            dt = DBOperator.QuerySql(sql);
             items = DBOperator.getListFromTable<TOTAL>(dt);
             ReportDataSource notdone = new ReportDataSource("dsNotDone", items);
             reportViewer1.LocalReport.DataSources.Add(notdone);
